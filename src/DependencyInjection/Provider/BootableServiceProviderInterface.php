df12d0ea39cea7a176ecf553f51559d380134261
Robert Kummer <post@robert-kummer.de>
2026-06-07 12:05:38 +0200

    feat: add service provider interfaces with boot phase

--- src/DependencyInjection/Provider/BootableServiceProviderInterface.php
+++ src/DependencyInjection/Provider/BootableServiceProviderInterface.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Hive\DependencyInjection\Provider;
 
+use Hive\DependencyInjection\Container;
+
+/**
+ * Provider with a boot phase. boot() is called AFTER all providers have
+ * been registered. Services may be resolved here.
+ */
 interface BootableServiceProviderInterface
 {
-
+    public function boot(Container $container): void;
 }
--- src/DependencyInjection/Provider/ServiceProviderInterface.php
+++ src/DependencyInjection/Provider/ServiceProviderInterface.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace Hive\DependencyInjection\Provider;
 
+use Hive\DependencyInjection\Container;
+
+/**
+ * A service provider encapsulates the registration of related services.
+ *
+ * register() is called during the registration phase. Only bindings may be
+ * registered here; services must not be resolved, as other providers may not
+ * yet be registered.
+ */
 interface ServiceProviderInterface
 {
-
+    public function register(Container $container): void;
 }
